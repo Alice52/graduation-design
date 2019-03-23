56c7fb9fb8f9e1aba1e8ae6f561a92cf45360ee8
Alice52 <zzhang_xz@163.com>
2019-03-23 23:29:48 +0800

    feat(gd): add models

--- zcEducation/apps/courses/models.py
+++ zcEducation/apps/courses/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from datetime import datetime
+from orgs.models import OrgsInfo, TeacherInfo
+
 
 # Create your models here.
+class CourseInfo(models.Model):
+    image = models.ImageField(upload_to='course/', max_length=200, verbose_name='课程封面')
+    name = models.CharField(verbose_name='课程名称', max_length=20)
+    study_time = models.IntegerField(verbose_name='课程时长', default=0)
+    study_num = models.IntegerField(verbose_name='学习人数', default=0)
+    level = models.CharField(choices=(('gj', '高级'), ('zj', '中级'), ('cj', '初级')), verbose_name='课程难度', max_length=5,
+                             default='cj')
+    love_num = models.IntegerField(verbose_name='收藏数', default=0)
+    click_num = models.IntegerField(verbose_name='点击数量', default=0)
+    desc = models.CharField(verbose_name='课程简介', max_length=200)
+    detail = models.CharField(verbose_name='课程详情')
+    category = models.CharField(choices=(('qd', '前端开发'), ('hd', '后端开发')), verbose_name='课程类别', max_length=5)
+    course_notice = models.CharField(verbose_name='课程公告', max_length=200)
+    course_need = models.CharField(verbose_name='课程须知', max_length=200)
+    teacher_tell = models.CharField(verbose_name="讲师提示", max_length=200)
+    orginfo = models.ForeignKey(OrgsInfo, verbose_name='所属机构')
+    teacherinfo = models.ForeignKey(TeacherInfo, verbose_name='所属讲师')
+    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '课程信息'
+        verbose_name_plural = verbose_name
+
+
+class LessonInfo(models.Model):
+    name = models.CharField(max_length=50, verbose_name="章节名称")
+    courseinfo = models.ForeignKey(CourseInfo, verbose_name="所属课程")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '章节信息'
+        verbose_name_plural = verbose_name
+
+
+class VideoInfo(models.Model):
+    name = models.CharField(max_length=50, verbose_name="视频名称")
+    study_time = models.IntegerField(default=0, verbose_name="视频时长")
+    url = models.URLField(default='http://www.atguigu.com', verbose_name="视频链接", max_length=200)
+    lessoninfo = models.ForeignKey(LessonInfo, verbose_name="所属章节")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '视频信息'
+        verbose_name_plural = verbose_name
+
+
+class SourceInfo(models.Model):
+    name = models.CharField(max_length=50, verbose_name="资源名称")
+    down_load = models.FileField(upload_to='source/', max_length=200, verbose_name="下载路径")
+    courseinfo = models.ForeignKey(CourseInfo, verbose_name="所属课程")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '资源信息'
+        verbose_name_plural = verbose_name
--- zcEducation/apps/orgs/models.py
+++ zcEducation/apps/orgs/models.py
@@ -1,3 +1,65 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+from django.db.models import DateTimeField
+
+
+class CityInfo(models.Model):
+    name = models.CharField(max_length=20, verbose_name='城市名称')
+    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '城市信息'
+        verbose_name_plural = verbose_name
+
+
+class OrgsInfo(models.Model):
+    image = models.ImageField(upload_to='org/', max_length=200, verbose_name='机构封面')
+    name = models.CharField(max_length=20, verbose_name='机构名称')
+    course_num = models.IntegerField(verbose_name='课程数', default=0)
+    study_num = models.IntegerField(verbose_name='学习人数', default=0)
+    address = models.CharField(max_length=200, verbose_name='机构地址')
+    desc = models.CharField(max_length=200, verbose_name='机构简介')
+    detail = models.CharField(verbose_name='机构详情')
+    love_num = models.IntegerField(verbose_name='收藏数', default=0)
+    click_num = models.IntegerField(verbose_name='点击数量', default=0)
+    category = models.CharField(choices=(('pxjg', '培训机构'), ('gx', '高校'), ('gr', '个人')), verbose_name='机构类别')
+    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+    cityinfo = models.ForeignKey(CityInfo, verbose_name='所属城市')
+    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+
+    def __str__(self):
+            return self.name
+
+    class Meta:
+        verbose_name = '机构信息'
+        verbose_name_plural = verbose_name
+
+
+class TeacherInfo(models.Model):
+    image = models.ImageField(upload_to='teacher/', max_length=200, verbose_name='讲师头像')
+    name = models.CharField(verbose_name='讲师姓名', max_length=20)
+    work_year = models.IntegerField(verbose_name='工作年限', default=3)
+    work_position = models.CharField(verbose_name='工作职位', max_length=20)
+    work_style = models.CharField(verbose_name='教学特点', max_length=20)
+    age = models.IntegerField(verbose_name='讲师年龄', default=30)
+    gender = models.CharField(choices=(('girl', '女'), ('boy', '男')), max_length=10, verbose_name='讲师性别', default='girl')
+    love_num = models.IntegerField(verbose_name='收藏数', default=0)
+    click_num = models.IntegerField(verbose_name='点击数量', default=0)
+    work_company = models.ForeignKey(OrgsInfo, verbose_name='所属机构')
+    add_time = models.DateTimeField(verbose_name='添加时间', default=datetime.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = '讲师信息'
+        verbose_name_plural = verbose_name
+
+
+
